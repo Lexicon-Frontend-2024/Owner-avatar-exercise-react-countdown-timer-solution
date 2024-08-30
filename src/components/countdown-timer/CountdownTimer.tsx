90577748c4ec19b7c0d2205aff06be000e829ac1
Niklas Fähnrich <niklasfahnrich@hotmail.com>
2024-08-30 11:15:18 +0200

    add feedback and exercise is complete

--- src/components/countdown-timer/CountdownTimer.tsx
+++ src/components/countdown-timer/CountdownTimer.tsx
@@ -3,6 +3,7 @@ import { Button } from "../button";
 
 export function CountdownTimer(): ReactElement {
   const [duration, setDuration] = useState<number>(60);
+  const [feedback, setFeedback] = useState<string>("");
   const [isActive, setIsActive] = useState<boolean>(false);
   const [timeLeft, setTimeLeft] = useState<number>(duration);
   const [value, setValue] = useState<number | string>("");
@@ -13,14 +14,21 @@ export function CountdownTimer(): ReactElement {
   const handleOnReset = () => {
     setIsActive(false);
     setTimeLeft(duration);
+    setFeedback("");
   };
 
   const handleOnSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
 
-    setDuration(value as number);
-    setTimeLeft(value as number);
-    setValue("");
+    if ((value as number) < 0) {
+      setDuration(0);
+      setTimeLeft(0);
+      setValue("");
+    } else {
+      setDuration(value as number);
+      setTimeLeft(value as number);
+      setValue("");
+    }
   };
 
   useEffect(() => {
@@ -28,18 +36,16 @@ export function CountdownTimer(): ReactElement {
       timerId.current = setInterval(() => {
         setTimeLeft((t) => t - 1);
       }, 1000);
-
-      console.log("Timer is running..");
     }
 
     if (isActive === false) {
       clearInterval(timerId.current);
-      console.log("Timer has stopped.");
     }
   }, [isActive]);
 
   useEffect(() => {
     if (timeLeft === 0) {
+      setFeedback("Game over!");
       setIsActive(false);
     }
   }, [timeLeft]);
@@ -51,7 +57,11 @@ export function CountdownTimer(): ReactElement {
           <h2>Timer</h2>
           <div className="actions">
             <Button icon="restart_alt" onClick={handleOnReset} />
-            <Button icon={computedCountdownState} onClick={() => setIsActive((ia) => !ia)} />
+            <Button
+              disabled={timeLeft <= 0}
+              icon={computedCountdownState}
+              onClick={() => setIsActive((ia) => !ia)}
+            />
           </div>
         </header>
         <h1 className="countdown">{timeLeft}</h1>
@@ -62,7 +72,7 @@ export function CountdownTimer(): ReactElement {
           <input onChange={(e) => setValue(parseInt(e.target.value))} type="number" value={value} />
         </form>
       </article>
-      <p className="feedback">Feedback</p>
+      <p className="feedback">{feedback}</p>
     </>
   );
 }
