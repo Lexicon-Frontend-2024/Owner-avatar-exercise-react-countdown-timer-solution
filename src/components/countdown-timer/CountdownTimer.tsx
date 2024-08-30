1621873b22692a12118369ace54e018bf3d9e83a
Niklas Fähnrich <niklasfahnrich@hotmail.com>
2024-08-30 09:59:16 +0200

    complete step 2 in exercise, create state variabels plus some extra

--- src/components/countdown-timer/CountdownTimer.tsx
+++ src/components/countdown-timer/CountdownTimer.tsx
@@ -1,7 +1,18 @@
-import { ReactElement } from "react";
+import { FormEventHandler, ReactElement, useState } from "react";
 import { Button } from "../button";
 
 export function CountdownTimer(): ReactElement {
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const [timeLeft, setTimeLeft] = useState<number>(80);
+  const [value, setValue] = useState<number | string>("");
+
+  const handleOnSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    setTimeLeft(value as number);
+    setValue("");
+  };
+
   return (
     <>
       <article id="countdown-timer">
@@ -12,12 +23,12 @@ export function CountdownTimer(): ReactElement {
             <Button icon="pause" />
           </div>
         </header>
-        <h1 className="countdown">60</h1>
-        <form className="duration">
+        <h1 className="countdown">{timeLeft}</h1>
+        <form className="duration" onSubmit={handleOnSubmit}>
           <label htmlFor="duration">
             Set duration <em>( seconds )</em>
           </label>
-          <input type="number" />
+          <input onChange={(e) => setValue(parseInt(e.target.value))} type="number" value={value} />
         </form>
       </article>
       <p className="feedback">Feedback</p>
